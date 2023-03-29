767b3d35c53b1d7bda5323ebe4becf7b37eae4d0
Thibaut Humblet <thibaut.humblet@uza.be>
2023-03-29 15:51:37 +0200

    Refactored code and tweaked appsettings

--- authPoc/Program.cs
+++ authPoc/Program.cs
@@ -6,27 +6,25 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var AllowAll = "_allowAll";
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-                      });
+    options.AddPolicy(name: AllowAll,
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
 });
 
-
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(AllowAll);
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -38,38 +36,37 @@ app.UseAuthorization();
 app.MapControllers();
 
 
-
 app.MapPost("/login", [AllowAnonymous] (User user) =>
 {
     var users = new List<User>
     {
-        new User { Id = 1, Email = "steven@uza.be", Password = "test123", Name = "Steven Van de Poel" },
-        new User { Id = 2, Email = "thibaut@uza.be", Password = "test123", Name = "Thibaut Humblet" }
+        new User { Id = 1, Email = "steven.vandepoel@uza.be", Password = "test123", Name = "Steven Van de Poel" },
+        new User { Id = 2, Email = "thibaut.humblet@uza.be", Password = "test123", Name = "Thibaut Humblet" },
+        new User { Id = 3, Email = "els.wittesaele@uza.be", Password = "test123", Name = "Els Wittesaele" },
     };
-
 
     if (users.Any(u => u.Email == user.Email && u.Password == user.Password))
     {
         var issuer = builder.Configuration["Jwt:Issuer"];
         var audience = builder.Configuration["Jwt:Audience"];
-        var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+        string? key = builder.Configuration.GetValue<string>("Jwt:Key");
+        key ??= string.Empty;
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(new[]
-    {
+            {
                 new Claim("Id", Guid.NewGuid().ToString()),
                 new Claim(JwtRegisteredClaimNames.Sub, user.Email),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti,
                 Guid.NewGuid().ToString())
-             }),
-            Expires = DateTime.UtcNow.AddMinutes(5),
+            }),
+            Expires = DateTime.UtcNow.AddMinutes(60),
             Issuer = issuer,
             Audience = audience,
-            SigningCredentials = new SigningCredentials
-    (new SymmetricSecurityKey(key),
-    SecurityAlgorithms.HmacSha512Signature)
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)), SecurityAlgorithms.HmacSha512Signature)
         };
+
         var tokenHandler = new JwtSecurityTokenHandler();
         var token = tokenHandler.CreateToken(tokenDescriptor);
         var jwtToken = tokenHandler.WriteToken(token);
@@ -79,15 +76,13 @@ app.MapPost("/login", [AllowAnonymous] (User user) =>
     return Results.Unauthorized();
 });
 
-
 app.Run();
-
 
 
 public class User
 {
     public int Id { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string Name { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 }
